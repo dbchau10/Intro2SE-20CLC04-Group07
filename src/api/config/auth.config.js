2fe47cd3149d756ad106bbfdaa01dc16d40c3f04
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2022-12-22 00:31:06 +0700

    create seeder for data inserting

---
+++ src/api/config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "cadger-secret-key"
+  };
--- src/api/index.js
+++ src/api/index.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cors = require("cors");
-const cookieSession = require("cookie-session");
 
 const app = express();
 
-app.use(cors());
+var corsOptions = {
+  origin: "http://localhost:8081"
+};
+
+app.use(cors(corsOptions));
 
 // parse requests of content-type - application/json
 app.use(express.json());
@@ -12,18 +15,16 @@ app.use(express.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(express.urlencoded({ extended: true }));
 
-app.use(
-  cookieSession({
-    name: "bezkoder-session",
-    secret: "COOKIE_SECRET", // should use as secret environment variable
-    httpOnly: true
-  })
-);
-
 // simple route
 app.get("/", (req, res) => {
-  res.json({ message: "Welcome to Cadger" });
+  res.json({ message: "Welcome to Cadger." });
 });
+app.get("/createTables", (req, res) => {
+   let models = require('./models');
+   models.sequelize.sync().then(() =>{
+    res.send('tables created');
+   });
+  });
 
 // set port, listen for requests
 const PORT = process.env.PORT || 8080;
