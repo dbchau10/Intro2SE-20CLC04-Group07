7a90323ee95cf2cc1d2f079564e55e923e8c9ec3
camphong24032002 <45125872+camphong24032002@users.noreply.github.com>
2022-12-22 22:04:09 +0700

    finish ItemDetail interface

--- src/Cadger/src/Screen/ItemDetail.js
+++ src/Cadger/src/Screen/ItemDetail.js
@@ -30,31 +30,55 @@ const data =
     title: 'Laptop cũ phục vụ học tập',
     status: 1,
     rating: 5.0,
+    ratingCnt: 20,
     borrowed: 200,
     desc: 'San pham tuyet voi',
     author: 'Messi',
   };
+// Load 3
+const RATING = [
+  {
+    id: 1,
+    author: 'Ronaldo',
+    rating: 5,
+    comment: 'Man hinh ro net',
+    date: '18/12/2022',
+  },
+  {
+    id: 2,
+    author: 'Ronaldinho',
+    rating: 5,
+    comment: 'May tinh dep lam nha, cam on em da cho anh muon de lam bai tap',
+    date: '20/12/2022',
+  },
+  {
+    id: 3,
+    author: 'Neymar',
+    rating: 5,
+    comment: '',
+    date: '22/12/2022',
+  },
+];
 
-// const Item = ({ item }) => (
-//   <View style={styles.itemBox}>
-//     <View style={styles.itemContentBox}>
-//       <Image source = {{uri:'https://cdn.pocket-lint.com/r/s/970x/assets/images/149624-laptops-review-hands-on-microsoft-surface-laptop-3-13-5-inch-review-image1-ndijeqz6fw.jpg'}}
-//       style = {{ width: '90%', height: '80%', alignSelf: 'center', marginVertical: '8%', borderRadius: 10, resizeMode: 'center'}}
-//       />  
-//     </View>
-//     <View style={styles.itemContentBox}>
-//       <Text style={styles.itemTitle}>{item.title}</Text>
-//       <View style={styles.itemStatusBox}>
-//         <Text style={styles.itemStatusText}>{item.status?'Available':'Unavailable'}</Text>
-//       </View>
-//       <Text style={styles.itemRating}>{item.rating} <Icon style={styles.eleIcon} name='star' size={16} color='yellow'/></Text>
-//       <Text style={styles.itemBorrowed}>{item.borrowed} borrowed</Text>
-//     </View>
-//   </View>
-// );
+const Item = ({ item }) => (
+  <View style={styles.ratingBox}>
+    <View style={styles.ratingImgBox}>
+      <Image source = {{uri:'https://media.discordapp.net/attachments/910154517046255656/1055499894166667304/image.png'}}
+      style = {{ width: 50, height: 50, alignSelf: 'center', marginVertical: '8%', borderRadius: 50, resizeMode: 'center'}}
+      />  
+    </View>
+    <View style={styles.ratingContentBox}>
+      <Text style={styles.ratingAuthor}>{item.author}</Text>
+      <View style={styles.ratingDetail}>
+        <Text style={styles.ratingPoint}>{item.rating}<Icon style={styles.eleIcon} name='star' size={16} color='yellow'/></Text>
+        <Text style={styles.ratingDate}>{item.date}</Text>
+      </View>
+      <Text style={styles.ratingCmt}>{item.comment}</Text>
+    </View>
+  </View>
+);
 
 const ItemDetail =  ({navigation, isLender}) => {
-  const [isSelected, setSelection] = React.useState(false);
   const renderItem = ({ item }) => (
     <Item item={item} />
   );
@@ -78,18 +102,42 @@ const ItemDetail =  ({navigation, isLender}) => {
               style={styles.btn}
               onPress={() => Alert.alert("Hello")}
               >
-              <Text style={styles.btnText}>Submit</Text>
-            </TouchableOpacity>}
+                <Text style={styles.btnText}>Borrow</Text>
+              </TouchableOpacity>
+              }
             </View>
           </View>
           <View style={styles.authorContainer}>
-
+            <Image source = {{uri:'https://media.discordapp.net/attachments/910154517046255656/1055499894166667304/image.png'}}
+              style = {styles.authorImg}
+              />
+            <View style={styles.authorContent}>
+              <Text style={styles.authorName}>{data.author}</Text>
+              <TouchableOpacity
+              style={styles.profileBtn}
+              onPress={() => Alert.alert("Hello")}
+              >
+                <Text style={styles.profileText}>Profile</Text>
+              </TouchableOpacity>
+            </View>
           </View>
           <View style={styles.descContainer}>
-
+              <View style={styles.descTitleBox}>
+                <Text style={styles.descTitle}>Description</Text>
+                <Text style={styles.borrowed}>{data.borrowed} borrowed</Text>
+              </View>
+              <Text style={styles.desc}>{data.desc}</Text>
           </View>
           <View style={styles.ratingContainer}>
-
+              <View style={styles.ratingTitleBox}>
+                <Text style={styles.ratingTitle}>Product Ratings</Text>
+                <Text style={styles.ratingText}><Icon style={{alignSelf: 'center',}} name='star' size={20} color='yellow'/> {data.rating}/5 ({data.ratingCnt})</Text>
+              </View>
+              <FlatList
+              data={RATING}
+              renderItem={renderItem}
+              keyExtractor={item => item.id}
+              />
           </View>
         </ScrollView>
         <View style={styles.navbar}>
@@ -167,6 +215,7 @@ const styles = StyleSheet.create({
     height: 0.25*windowHeight,
     width: 0.8*windowWidth,
     alignSelf: 'center',
+    borderRadius: 20,
   },
   title: {
     fontSize: 25,
@@ -179,15 +228,109 @@ const styles = StyleSheet.create({
     marginVertical: 15,
     height: 0.15*windowHeight,
     backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  authorImg: {
+    width: 80,
+    height: 80,
+    borderRadius: 80,
+    alignSelf: 'center',
+    marginHorizontal: 50,
+  },
+  authorContent: {
+    paddingTop: 20,
+  },
+  authorName: {
+    fontSize: 20,
+    color: 'black',
+    fontWeight: 'bold',
+    width: 200,
+    maxHeight: 80,
+    paddingBottom: 8,
+  },
+  profileBtn: {
+    height: 22,
+    width: 150,
+    backgroundColor: green,
+    borderRadius: 10,
+    borderWidth: 1,
+  },
+  profileText: {
+    font: 8,
+    color: 'white',
+    textAlign: 'center',
   },
   descContainer: {
     minHeight: 0.2*windowHeight,
     backgroundColor: 'white',
+    paddingHorizontal: 20,
+  },
+  descTitleBox: {
+    height: 40,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingTop: 15,
+  },
+  descTitle: {
+    alignSelf: 'center',
+    fontWeight: 'bold',
+    color: 'black',
+    fontSize: 20,
+  },
+  borrowed: {
+    alignSelf: 'center',
+    color: 'black',
+  },
+  desc: {
+    fontSize: 15,
+    paddingTop: 10,
+    color: 'black',
   },
   ratingContainer: {
-    height: 0.45*windowHeight,
+    minHeight: 0.08*windowHeight,
     marginTop: 15,
     backgroundColor: 'white',
+  },
+  ratingTitleBox: {
+    paddingTop: 15,
+    height: 80,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+  },
+  ratingTitle: {
+    color: 'black',
+    fontSize: 20,
+    fontWeight: 'bold',
+  },
+  ratingBox: {
+    minHeight: 80,
+    borderTopWidth: 1,
+    flexDirection: 'row',
+    borderColor: 'lightgray',
+  },
+  ratingImgBox: {
+    width: 0.3*windowWidth,
+    marginRight: -20,
+  },
+  ratingAuthor: {
+    paddingTop: 5,
+    color: 'black',
+    fontWeight: 'bold',
+  },
+  ratingDetail: {
+    flexDirection: 'row',
+  },
+  ratingPoint: {
+    color: 'black',
+  },
+  ratingDate: {
+    paddingLeft: 10,
+  },
+  ratingCmt: {
+    width: 250,
+    color: 'black',
+    marginBottom: 10,
   },
   btnContainer: {
     height: 0.08*windowHeight,
@@ -202,6 +345,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     borderRadius: 20,
     marginHorizontal: 30,
+    borderWidth: 1,
   },
   btnText: {
     textAlign: 'center',
