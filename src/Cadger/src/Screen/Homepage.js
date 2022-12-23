cb370764821ffbb7a20c28320a649d923b9138c4
dbchau10 <82007790+dbchau10@users.noreply.github.com>
2022-12-23 20:29:01 +0700

    add navbar

--- src/Cadger/src/Screen/Homepage.js
+++ src/Cadger/src/Screen/Homepage.js
@@ -16,8 +16,9 @@ import {
 } from 'react-native';
 const windowWidth = Dimensions.get('window').width;
 const windowHeight = Dimensions.get('window').height;
-
+const green = '#98FB98';
 import Icon from 'react-native-vector-icons/FontAwesome';
+import Icon2 from 'react-native-vector-icons/Ionicons';
 import {parameters} from '../global/style';
 import { ItemData } from '../global/data';
 import ItemCard from '../components/ItemCard';
@@ -53,16 +54,46 @@ const Homepage = () => {
               />
             </View>
           </View>
-           </ScrollView>
+        
 
            <View>
+            <View style={styles.textHeadingBox}>
+            <Text style={styles.textHeading}>Top Products</Text>
+            <Text>View All</Text>
+            </View>
           <FlatList
             data={ItemData}
             keyExtractor={item => item.id}
             renderItem={ItemCard}
           />
         </View>
+       
         <StatusBar style="light" backgroundColor="#33a333" translucent={true} />
+        </ScrollView>
+        <View style={styles.navbar}>
+            <View style={styles.eleBox}>
+                <Icon2 style={styles.eleIcon} name='home-outline' size={35} color='black' />
+                <Text style={styles.eleText}>Home</Text>
+            </View>
+            <View style={styles.eleBox}>
+                <Icon2 style={styles.eleIcon} name='mail-outline' size={35} color='black' />
+                <Text style={styles.eleText}>Request</Text>
+            </View>
+            <View style={styles.eleBox}>
+                <View style={[styles.eleIcon,
+                {width: 60, height: 60, backgroundColor: green, marginBottom: 60, borderRadius: 60, justifyContent: 'center', alignItems: 'center', borderWidth: 3, borderColor: 'white'}]}>
+                  <Icon styles={styles.eleText} name='plus' size={35} color='white' />
+                </View>
+            </View>
+            <View style={styles.eleBox}>
+                <Icon2 style={styles.eleIcon} name='notifications-outline' size={35} color='black' />
+                <Text style={styles.eleText}>Notification</Text>
+            </View>
+            <View style={styles.eleBox}>
+                <Icon2 style={styles.eleIcon} name='person-outline' size={35} color='black' />
+                <Text style={styles.eleText}>Account</Text>
+            </View>
+        </View>
     </SafeAreaView>
   );
 };
@@ -95,7 +126,17 @@ const styles = StyleSheet.create({
     height: 50,
     borderRadius: 50
   },
-
+  textHeadingBox: {
+    padding: 20,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center'
+  },
+  textHeading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    color: 'black'
+  },
   text1: {
     color: "white"
   },
@@ -136,6 +177,37 @@ const styles = StyleSheet.create({
   searchArea: {
     alignItems: 'center',
     zIndex: 1
-  }
+  },
+  navbar: {
+    height: 0.1*windowHeight,
+    width: windowWidth,
+    borderTopLeftRadius: 20,
+    borderTopRightRadius: 20,
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 1,
+      height: 10,
+    },
+    shadowOpacity: 0.5,
+    shadowRadius: 12.35,
+    elevation: 18,
+    flexDirection: 'row',
+    position: 'absolute',
+    bottom: 0,
+    left: 0,
+    backgroundColor: 'white'
+  },
+  eleBox: {
+    width: 0.2*windowWidth,
+    height: '100%',
+    justifyContent: 'center',
+  },
+  eleIcon: {
+    alignSelf: 'center',
+  },
+  eleText: {
+    textAlign: 'center',
+    color: 'black',
+  },
   
 })
--- src/Cadger/src/global/data.js
+++ src/Cadger/src/global/data.js
@@ -23,5 +23,30 @@ export const ItemData = [
     borrowed: 200,
     imagePath: require('../../assets/pictures/book.jpg')
 
+  },
+  {
+    id: 4,
+    title: 'Laptop cũ phục vụ học tập',
+    status: 1,
+    rating: 5.0,
+    borrowed: 220,
+    imagePath: require('../../assets/pictures/laptop.jpg')
+  },
+  {
+    id: 5,
+    title: 'Camera chụp cực nét',
+    status: 1,
+    rating: 5.0,
+    borrowed: 210,
+    imagePath: require('../../assets/pictures/camera.jpg')
+  },
+  {
+    id: 6,
+    title: 'Sách làm lại cuộc đời',
+    status: 0,
+    rating: 5.0,
+    borrowed: 200,
+    imagePath: require('../../assets/pictures/book.jpg')
+
   }
 ];
