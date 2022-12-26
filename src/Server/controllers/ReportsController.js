3ca8221f6b1ca9eaf8f35b41084f5f4b8f7b31d9
NgKhaiPhu <nkphu243373@gmail.com>
2022-12-26 22:22:40 +0700

    handling report
    router + controller

---
+++ src/Server/controllers/ReportsController.js
@@ -0,0 +1,46 @@
+const { json } = require('express');
+const pool = require("../models/database");
+
+class ReportsController {
+    // [GET]
+    async read(req, res) {
+        try {
+            const report = await pool.query(
+                "SELECT * FROM report"
+            );
+            res.json(report.rows[0]);
+        }
+        catch (err){
+            console.error(err.message);
+        }
+    }
+    // [POST]
+    async create(req, res) {
+        try {
+            const data = req.body;
+            const report = await pool.query(
+                "INSERT INTO report(reason, sender, reported) VALUES($1, $2, $3) RETURNING *",
+                [data.reason, data.sender, data.reported]
+            );
+            res.json(report.rows[0]);
+        }
+        catch (err){
+            console.error(err.message);
+        }
+    }
+    // [DELETE]
+    async delete(req, res) {
+        try {
+            const { id } = req.params;
+            const report = await pool.query(
+                "DELETE FROM report WHERE report_id = $1 RETURNING *",
+                [id]
+            );
+            res.json(report.rows[0]);
+        }
+        catch (err){
+            console.error(err.message);
+        }
+    }
+}
+module.exports = new ReportsController;
---
+++ src/Server/routes/reports.js
@@ -0,0 +1,9 @@
+const express = require('express');
+const router = express.Router();
+const reportsController = require('../controllers/ReportsController');
+
+router.get('/', reportsController.read);
+router.post('/create', reportsController.create);
+router.delete('/delete/:id', reportsController.delete);
+
+module.exports = router;
