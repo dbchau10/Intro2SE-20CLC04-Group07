92ee69d9da0807db7b36289dc72c0f33870012ef
vdkhoi20 <vdkhoi.itai@gmail.com>
2023-01-01 06:10:24 +0700

    add router send email

--- src/Server/routes/index.js
+++ src/Server/routes/index.js
@@ -1,6 +1,7 @@
 const accountRouter = require('./accounts');
 const borrowrequestRouter = require('./borrowrequests');
 const itemRouter = require('./items');
+const emailRouter=require('./email')
 // const logRouter = require('./logs');
 // const notificationRouter = require('./notifications');
 // const ratingRouter = require('./ratings');
@@ -8,10 +9,11 @@ const itemRouter = require('./items');
 // const returnRouter = require('./returns');
 
 function route(app) {
-
+    
     app.use('/accounts', accountRouter);
     app.use('/borrowrequests', borrowrequestRouter);
     app.use('/items', itemRouter);
+    app.use('/sendemail',emailRouter)
     // app.use('/logs', logRouter);
     // app.use('/notifications', notificationRouter);
     // app.use('/ratings', ratingRouter);
