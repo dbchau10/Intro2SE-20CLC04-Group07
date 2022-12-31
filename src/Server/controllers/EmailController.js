2d7f8999024b8ca866204d6011a8567f68688ba5
vdkhoi20 <vdkhoi.itai@gmail.com>
2023-01-01 06:22:20 +0700

    changing log message

--- src/Server/controllers/EmailController.js
+++ src/Server/controllers/EmailController.js
@@ -7,8 +7,9 @@ class EmailController {
         try {
             //res.send("hit");
             const {to_email,content} = req.params;
-            await send_email(to_email,content).catch(console.error)
-            res.send(content);
+            await send_email(to_email,content).catch(err=>{
+                res.send(`some error occurs ${err.message} can't send email to ${to_email}`)
+            }).then(res.send(`sending email to ${to_email} with content ${content} successfully`))
         }
         catch (err){
             console.error(err.message);
